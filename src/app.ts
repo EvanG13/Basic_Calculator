5e725e505ab7ef00416a1f4fadac2395e2baf50e
EvanG13 <emgiampaoli@gmail.com>
2023-04-09 08:54:45 -0700

    basic calculator html structure

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import path from 'path';
 const app = express();
 const port: number = 3000;
 
+// serve static files in dist directory
 app.use(express.static(path.join(__dirname + '/../dist')));
+// serve static files in public directory
 app.use(express.static(path.join(__dirname + '/../public/css')));
 app.use(express.static(path.join(__dirname + '/../public/html')));
 
